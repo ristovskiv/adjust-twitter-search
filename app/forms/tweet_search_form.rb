8264ade72a3279eb507a93a2caed90675388c49c
Vlatko Ristovski <ristovski.vlatko234@gmail.com>
2018-05-19 21:56:46 +0200

    feat: introduce TweetSearchForm for searching tweets

---
+++ app/forms/tweet_search_form.rb
@@ -0,0 +1,23 @@
+class TweetSearchForm
+  include ActiveModel::Model
+
+  attr_accessor :q
+
+  validate :format_of_search_term
+
+  def self.model_name
+    ActiveModel::Name.new(self, nil, 'TweetSearch')
+  end
+
+  def call(adapter: TwitterAdapter.new)
+    return [] if q.blank?
+    return [] unless valid?
+    adapter.search(q)
+  end
+
+  private
+
+  def format_of_search_term
+    errors.add(:q) if q.match(/\A(_|\W)+\z/)
+  end
+end
---
+++ spec/forms/tweet_search_form_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe TweetSearchForm do
+
+  it { is_expected.to respond_to(:q) }
+  it { is_expected.to respond_to(:q=) }
+  it { is_expected.to respond_to(:call) }
+
+  context 'validations' do
+    describe '#q' do
+      it 'must be meaningful' do
+        unmeaningful_term = %w(_ + = & # ! ?).sample * [*1..9].sample
+        tweet_search_form = described_class.new(q: unmeaningful_term)
+        tweet_search_form.valid?
+
+        expect(tweet_search_form.errors[:q]).to include(match(/is invalid/))
+      end
+    end
+  end
+
+  context '#call' do
+    describe 'when term is blank' do
+      it 'returns an empty collection' do
+        tweet_search_form = described_class.new(q: '')
+
+        expect(tweet_search_form.call).to be_empty
+      end
+    end
+
+    describe 'when form is not valid' do
+      it 'returns and empty collection' do
+        tweet_search_form = described_class.new(q: 'adjust')
+        allow(tweet_search_form).to receive(:valid?).and_return(false)
+
+        expect(tweet_search_form.call).to be_empty
+      end
+    end
+
+    describe 'when form is valid' do
+      it 'can get collection of tweets' do
+        adapter = instance_double(TwitterAdapter)
+        expect(adapter).to receive(:search).with('adjust')
+
+        tweet_search_form = described_class.new(q: 'adjust')
+        allow(tweet_search_form).to receive(:valid?).and_return(true)
+
+        tweet_search_form.call(adapter: adapter)
+      end
+    end
+  end
+
+end
