063e07c41f203a9131ba72787de5cca4f7a94d14
Vlatko Ristovski <ristovski.vlatko234@gmail.com>
2018-05-19 21:57:39 +0200

    feat: introduce tweets#index search form for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
   def index
+    @tweet_search_form = TweetSearchForm.new(tweet_search_form_params)
+    @tweets = @tweet_search_form.call
+  end
+
+  private
+
+  def tweet_search_form_params
+    params.fetch(:tweet_search, {}).permit(:q)
   end
 end
--- spec/features/tweets_index_spec.rb
+++ spec/features/tweets_index_spec.rb
@@ -1,8 +1,38 @@
 require "rails_helper"
 
 RSpec.feature "Tweets index" do
-  scenario "when visiting the page" do
+  background do
     visit tweets_path
+  end
+
+  scenario "when visiting the page" do
     expect(page).to have_content('Tweets')
+
+    expect(find('ul#tweets')).to_not have_selector('li')
+  end
+
+  scenario 'when searching with no term' do
+    fill_in id: 'tweet_search_q', with: ''
+    click_button 'Search'
+
+    expect(find('ul#tweets')).to_not have_selector('li')
+  end
+
+  scenario 'when searching with invalid term' do
+    fill_in id: 'tweet_search_q', with: %w(_ + = & # ! ?).sample * [*1..9].sample
+    click_button 'Search'
+
+    expect(find('ul#tweets')).to_not have_selector('li')
+
+    expect(find('#tweet_search_q + span.error')).to have_text('is invalid')
+  end
+
+  scenario 'when searching with valid term' do
+    VCR.use_cassette('twitter_search/q_adjust_count_10') do
+      fill_in id: 'tweet_search_q', with: 'adjust'
+      click_button 'Search'
+
+      expect(find('ul#tweets')).to have_selector('li', count: 10)
+    end
   end
 end
